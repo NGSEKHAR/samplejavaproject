537bc6136413c54994bfed08afbcd2ceb68bca97
sekhar <ngunasekhar24@gmail.com>
2017-09-15 19:55:00 +0530

    adding hello world java project

--- hello.java
+++ hello.java
@@ -1,12 +1,7 @@
 public class hello{
-	
-public static void main(String[] arg) {
-	
-		for(int i=1;i<=10;1++){
-			
+	public static void main(String[] arg){
+	for(int i=1;i<=10;1++){
 			system.out.println("hello world.."+i);
 		}
-	
 	}
-
-}	
+}
